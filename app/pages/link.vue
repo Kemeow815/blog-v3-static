4c87072938d8bcc9d3aaa208b19327e11baa6f04
Kemeow815 <kemiao@kmblog.icu>
2025-04-17 13:16:53 +0800

    fix: update link page, change QQ group to TG group

--- app/pages/link.vue
+++ app/pages/link.vue
@@ -29,8 +29,8 @@ const { data: postLink } = await useAsyncData('/link', () => queryContent('/link
         <div class="content">
             <p><Icon name="ph:newspaper-clipping-bold" /> 我会通过订阅源阅读友链文章。</p>
             <p>
-                欢迎加入 QQ 群 <Tip copy>
-                    {{ appConfig.qqGroup }}
+                欢迎加入 TG 群 <Tip copy>
+                    {{ appConfig.tgGroup }}
                 </Tip> 闲聊或技术交流。
             </p>
             <p>
