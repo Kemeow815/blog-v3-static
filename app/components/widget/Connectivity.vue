2d6deb78712b405942b81aacb77fe9e32c35dbaa
Kemeow815 <kemiao@kmblog.icu>
2025-04-17 13:44:20 +0800

    add annoucement

--- app/components/widget/Connectivity.vue
+++ app/components/widget/Connectivity.vue
@@ -4,7 +4,7 @@ const appConfig = useAppConfig()
 
 <template>
     <h3 class="widget-title">
-        可访问性
+        公告
     </h3>
     <div
         class="widget-card seasonal"
@@ -14,12 +14,11 @@ const appConfig = useAppConfig()
         }"
     >
         <!-- TODO: 优化技术架构展示 -->
-        <p>域名启用 DNSSEC 和 HSTS，续费至2030年8月，托管于 Vercel；博客由 Nuxt Content 驱动，使用去图图床。</p>
+        <p>感谢纸鹿的nuxt主题开源！托管于 Vercel；使用<sm class="ms"></sm>图床。</p>
         <p>
-            获得省级、国家级认证如下：<br>
-            江苏:<span class="seasonal-emoji">跳转反诈宣传</span><br>
-            重庆:<span class="seasonal-emoji">DNS污染</span><br>
-            移动:<span class="seasonal-emoji">多省阻断解析</span>
+            分享克喵的生活记录、资源分享和踩坑教程。<br>
+            分享开源项目、软件、网站等等，侵删。<br>
+            转载加出处，谢谢。
         </p>
     </div>
 </template>
