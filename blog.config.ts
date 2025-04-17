82c2fc28112c652583edec3b350786b290971890
Kemeow815 <kemiao@kmblog.icu>
2025-04-17 17:27:41 +0800

    fix: update blog config description

--- blog.config.ts
+++ blog.config.ts
@@ -11,7 +11,7 @@ const blogConfig = {
     title: '喵落阁',
     subtitle: '总有些事情高于其他！',
     // 长 description 利好于 SEO
-    description: '克喵的个人博客，取自纸鹿的主题。',
+    description: '克喵的个人博客，取自纸鹿的主题。feedId:135622467121437696+userId:127507323861832704',
     author: {
         name: '克喵爱吃卤面',
         avatar: 'https://cravatar.cn/avatar/7614B285E795F21E780247019C4E15C4',
